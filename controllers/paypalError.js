37322e9d2aa86f8bcdff508284b4e22641402a88
Semal Mahajan <64732701+semalM20@users.noreply.github.com>
2024-06-15 13:06:48 +0530

    Update paypalError.js

--- controllers/paypalError.js
+++ controllers/paypalError.js
@@ -1,5 +1,6 @@
 const payFailed = async (req, res) => {
-  return res.redirect(`${process.env.fRONTEND_URL}/failed`);
+  // return res.redirect(`${process.env.fRONTEND_URL}/failed`);
+  return res.send(`${process.env.fRONTEND_URL}/failed`);
 };
 
 module.exports = payFailed;
