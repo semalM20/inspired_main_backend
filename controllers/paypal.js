8c767a4e7904d7c179445b17995073bb56d8c916
Semal Mahajan <64732701+semalM20@users.noreply.github.com>
2024-06-17 16:52:17 +0530

    Update paypal.js

--- controllers/paypal.js
+++ controllers/paypal.js
@@ -1,7 +1,8 @@
 const paypal = require("paypal-rest-sdk");
 
 paypal.configure({
-  mode: "sandbox",
+  // mode: "sandbox",
+  mode: "live",
   client_id: process.env.CLIENT_ID,
   client_secret: process.env.CLIENT_SECRET,
 });
